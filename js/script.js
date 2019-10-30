bf0cf6390ec47e68c2d1d47d80d09b5cc0961851
Jason Hill <Jason161@gmail.com>
2019-10-29 18:31:21 -0700

    working, for meets expectations
    Working for meets expectations.  Have not refactored or added extra functionality for exceeds expectations.

--- js/script.js
+++ js/script.js
@@ -5,43 +5,18 @@ FSJS project 2 - List Filter and Pagination
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// GOING FOR EXCEEDS EXPECTATIONS, PLEASE RETURN IF NEEDS WORK.
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const studentList = document.querySelectorAll(".student-item");
-//const list = document.getElementsByClassName(".student-item");
-console.log(studentList);
+//console.log(studentList);
 const itemsPerPage = 10;
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 function showPage (list, page) {
 let startIndex = (page * itemsPerPage) - itemsPerPage;
 let endIndex = page * itemsPerPage;
 for (let i = 0; i < list.length; i ++) {
-      if (i >= startIndex && i <= endIndex) {
+      if (i >= startIndex && i < endIndex) {
          list[i].style.display = '';                  
       } else list[i].style.display = 'none';
 
@@ -49,54 +24,40 @@ for (let i = 0; i < list.length; i ++) {
       
    };
 
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
+// Append function to create the div, ul, and li, and append in correct order
 function appendPageLinks (list) {
    const body = document.querySelector('.page');
    const createDiv = document.createElement('div');
    const createUl = document.createElement('ul');
    const createLi = document.createElement('li');
 
-// Div pagination class naming and appending
-   createDiv.className = "pagination";
+   createDiv.className = "pagination"; // Div pagination class naming and appending
    body.appendChild(createDiv);
+   createDiv.appendChild(createUl); // UL appending   
 
-// UL appending   
-   createDiv.appendChild(createUl);
 
-// LI creation loop, anchor creation, text content adding, and passing showPage
-   
+// LI creation loop, anchor creation, text content adding, class naming
    for (let i = 0; i < studentList.length / 10; i ++) {
-     //let li = document.createElement('li')
      let a = document.createElement('a');
-     //let firstA = document.querySelector('a');
      createUl.appendChild(createLi)
      createLi.appendChild(a);
      document.querySelector('a').className = "active";
      a.href = "#";
      a.textContent = i + 1;
-     a.addEventListener('click', showPage(i)) 
-      for (let i = 0; i < studentList.length / 10; i ++) {
-         document.querySelector("a").classList.remove("active")
-      }
-     click.target.className = "active";
+     let aList = document.querySelectorAll('a'); // variable selecting all anchor elements
 
-   }
+      for (let j = 0; j < aList.length; j ++) { // for loop to iterate through all the anchors, using aList variable set above
+         document.addEventListener('click', (event) => { // event handler, uses 'listener (event)', then can use event.target
+         showPage(list, event.target.textContent); // calls showPage on list again, but with page being Anchor element of event
+         document.querySelectorAll('a').className = '';
+         aList[j].className = "active";
+         });
+      }; 
+      
+   };
+};
+showPage(studentList, 1);
+appendPageLinks(studentList);
 
 
 
-   
-}
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
